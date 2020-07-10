9646a041fb73aa9c6d94d32ddf9505fe777c33c3
khembach <3229704+khembach@users.noreply.github.com>
2020-07-10 18:26:17 +0200

    fix indentation

--- data/Lam_figure2_markers.R
+++ data/Lam_figure2_markers.R
@@ -1,5 +1,8 @@
-fs <- list(Neural_progenitor = c("PRDX1", "MEG3", "NME2", "SNX2", "MARCKS", "NES", "PTX3", "VIM", "SOX11", "ID2", "CXCR4", "CD24"),
-           Neural_prog_proliferating = c("HIST1H3C", "TOP2A", "CHCHD2", "CDK1", "MIF"),
+fs <- list(Neural_progenitor = c("PRDX1", "MEG3", "NME2", "SNX2", "MARCKS", 
+                                 "NES", "PTX3", "VIM", "SOX11", "ID2", "CXCR4", 
+                                 "CD24"),
+           Neural_prog_proliferating = c("HIST1H3C", "TOP2A", "CHCHD2", "CDK1", 
+                                         "MIF"),
            Radial_glia_progenitor = c("SOX3", "CDH2", "FABP7", "HES5", "PAX6"),
            Glia_progenitor = c("SLC2A3", "SEMA3C", "LPL", "COL3A1", "S100B"),
            Neuroblast = c("DLL3", "DLL1", "DCX", "HES6", "SOX4", "NFASC"))
